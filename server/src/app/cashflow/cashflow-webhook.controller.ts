80110aa51255ba81e4a2eb255390a5faada8d133
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2026-02-06 11:07:17 +0700

    Cashflow webhook

--- server/src/app/cashflow/cashflow-webhook.controller.ts
+++ server/src/app/cashflow/cashflow-webhook.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { WebhookGuard } from '../../guards/webhook.guard';
 import { CashflowFormService } from './services/cashflow-form.service';
 import { CashflowDto } from './dto/cashflow.dto';
@@ -8,39 +8,13 @@ import { CashflowDto } from './dto/cashflow.dto';
 export class CashflowWebhookController {
   constructor(private readonly cashflowFormService: CashflowFormService) {}
 
-  @Get('transfer-to-rdn')
-  async createNewTransferRdnCashflow() {
-    const payload: CashflowDto = {
-      _validateTransferRule: true,
-      category: {
-        name: 'Transfer Internal',
-        status: 'transfer',
-      },
-      price: 10000,
-      product_service: 'Perisapan Investasi',
-      transaction_at: new Date().toISOString(),
-      from_asset: 'Seabank',
-      to_asset: 'RDN Bibit',
-      note:"Otomatis dari Webhook"
-    };
+  @Post('transfer-to-rdn')
+  async createNewTransferRdnCashflow(@Body() payload:CashflowDto) {
     return await this.cashflowFormService.createNewCashflowData(payload);
   }
 
-  @Get('transfer-to-rdpu')
-  async createNewSipCashflow() {
-    const payload: CashflowDto = {
-      _validateTransferRule: true,
-      category: {
-        name: 'Investasi',
-        status: 'transfer',
-      },
-      price: 10000,
-      product_service: 'Investasi RDPU',
-      transaction_at: new Date().toISOString(),
-      from_asset: 'RDN Bibit',
-      to_asset: 'Bahana Likuid Syariah Kelas G',
-      note:"Otomatis dari Webhook"
-    };
+  @Post('transfer-to-rdpu')
+  async createNewSipCashflow(@Body() payload:CashflowDto) {
     return await this.cashflowFormService.createNewCashflowData(payload);
   }
 }
